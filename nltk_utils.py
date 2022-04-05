525fd77b746fb303a5d56b48e574a9931d3dd607
bistcuite <85484922+bistcuite@users.noreply.github.com>
2022-04-05 21:05:38 +0430

    Add source code

---
+++ nltk_utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+import hazm
+
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    stemmer = hazm.Stemmer()
+    # stem each word
+    sentence_words = [stemmer.stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
+    return bag
